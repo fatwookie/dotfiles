ee6757d9897e29c8da3c1574fd2089e63deb1c5e
Vincent Bodenstab <github@bodenstab.net>
2021-06-01 23:25:51 +0200

    Bumped Golang to 1.14

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
 # stuff needed for Golang
 # on ubuntu 20.04lts, the Go binary is installed under /usr/lib
 set -x GOPATH /home/vincent/Devel/GoWorkspace/
-set -x GOROOT /usr/lib/go-1.13
+set -x GOROOT /usr/lib/go-1.14
